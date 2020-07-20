02ff9e9701f80a57884fefbd494e78682e8d7785
junevil0603 <79992819@qq.com>
2020-07-21 06:15:22 +0800

    路由器控制台入口

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <windows.h>
+
+/*
+1.ÍøÕ¾404¹¥»÷
+2.ÍøÕ¾´Û¸Ä¹¥»÷
+3.ÍøÕ¾¹¥»÷¼ÇÂ¼
+4.DNS¹¥»÷
+5.·şÎñÆ÷ÖØÆô¹¥»÷
+*/
+int main (void){
+	char name;
+	int  pwd;
+
+	std::cout <<"ÇëÊäÈëÕÊºÅ£º";
+	std::cin >>name;
+	std::cout <<"ÇëÊäÈëÃÜÂë£º";
+	std::cin >>pwd;
+
+	/*
+	std::cout << "1.ÍøÕ¾404¹¥»÷" << std::endl;
+	std::cout << "2.ÍøÕ¾´Û¸Ä¹¥»÷" << std::endl;
+	std::cout << "3.ÍøÕ¾¹¥»÷¼ÇÂ¼" << std::endl;
+	std::cout << "4.DNS¹¥»÷" << std::endl;
+	std::cout << "5.·şÎñÆ÷ÖØÆô¹¥»÷" << std::endl;
+	*/
+	system("pause");
+
+	return 0;
+}
